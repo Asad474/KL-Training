5eeb661edd15740cdbee58e27b81f0972ba10d84
Asad Sarfaraz <asadsarfraz474@gmail.com>
2024-08-22 19:33:05 +0530

    Day5

--- Day5/index.js
+++ Day5/index.js
@@ -1,8 +1,15 @@
 // 1) Initialise Git Repository (git init)
-// 2) git add . (For tracking all files that have changed)
+// 2) Add changes to staging environment (temporary storage)
+//    git add . (For tracking all files that have changed)
 //    git add "file_name" (For tracking particular file that have changed) 
 //    git add "file1" "file2" "file3" "file4"
+// 3) Commit those changes from staging enviroment means make changes permanent.
+//    git commit -m "Any Message"
+// 4) At any point if you want to check how many files have been staged or commited
+//    git status
 
 console.log('Message');
 console.log('H');
 console.log('World');
+console.log('K');
+console.log('H');

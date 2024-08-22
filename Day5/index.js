591e2ea31e68798a634682b261cc0cb4dbfd7be8
Asad Sarfaraz <asadsarfraz474@gmail.com>
2024-08-22 19:10:54 +0530

    Consoling

--- Day5/index.js
+++ Day5/index.js
@@ -2,3 +2,5 @@
 // 2) git add . (For tracking all files that have changed)
 //    git add "file_name" (For tracking particular file that have changed) 
 //    git add "file1" "file2" "file3" "file4"
+
+console.log('Message');

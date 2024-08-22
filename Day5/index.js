1bdc16b04d3ad0eaa7f6b534abf94facf8b76e86
Asad Sarfaraz <asadsarfraz474@gmail.com>
2024-08-22 19:08:50 +0530

    Creating index.html and index.js files

---
+++ Day5/index.js
@@ -0,0 +1,4 @@
+// 1) Initialise Git Repository (git init)
+// 2) git add . (For tracking all files that have changed)
+//    git add "file_name" (For tracking particular file that have changed) 
+//    git add "file1" "file2" "file3" "file4"

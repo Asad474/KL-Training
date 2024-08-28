a28ce0109eb328a5a5cb91ed188c2bdff8e0f9bb
Asad Sarfaraz <asadsarfraz474@gmail.com>
2024-08-28 19:26:27 +0530

    Creating nodejs server.

--- Day6/index2.js
+++ Day6/index2.js
@@ -1,1 +1,10 @@
-console.log('Index2 js file.');
+// Buildin Modules: Modules that are installed with nodejs
+// Custom Modules: Modules that are defined by users.
+
+function displayMessage() {
+    console.log('Hello KL');
+}
+
+module.exports = { 
+    displayMessage
+};

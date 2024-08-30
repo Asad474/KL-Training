4708383a2cc13637fd21d0435591cf9f4940f5de
Asad Sarfaraz <asadsarfraz474@gmail.com>
2024-08-30 19:44:28 +0530

    Middleware and API

--- Day7/server.js
+++ Day7/server.js
@@ -6,8 +6,20 @@ const app = express();
 // Routing /, /login, /register.
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+//Build in Middleware
+//For fetching post form data
+app.use(express.urlencoded({ extended: true }));
+
+//Custom Middleware
+const loginMiddleware = (req, res, next) => {
+    console.log('User authenticated.');
+    next();
+}
+
+app.get('/', loginMiddleware, (req, res) => {
+    console.log('Index');
     res.render('index');
+    // res.send('Hello');
 })
 
 app.get('/login', (req, res) => {
@@ -15,6 +27,16 @@ app.get('/login', (req, res) => {
     res.render('login');
 })
 
+app.get('/user', (req, res) => {
+    res.render('user');
+})
+
+app.post('/login', (req, res) => {
+    console.log('Post');
+    console.log(req.body);
+    res.redirect('/user');
+})
+
 app.listen(8000, () => {
-    console.log('Server is running at port 8080');
+    console.log('Server is running at port 8000');
 })

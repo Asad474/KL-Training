83eac4c7df720157014fc65e2c3fa3dffc01a29c
Asad Sarfaraz <asadsarfraz474@gmail.com>
2024-08-31 19:26:44 +0530

    Express with ejs practice

---
+++ Day8/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+// Middleware for setting CSS, images files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware for parsing body
+app.use(express.urlencoded({ extended: true }));
+
+const port = 8080;
+let contact_obj = {};
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+})
+
+app.post('/contact', (req, res) => {
+    console.log(req.body);
+    contact_obj = req.body;
+    console.log(contact_obj);
+    res.redirect('/user');
+})
+
+app.get('/user', (req, res) => {
+    res.render('user', contact_obj);
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening at port ${port}`);
+})

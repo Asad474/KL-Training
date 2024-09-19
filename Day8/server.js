2f927d80d006c0f84ff20077fafb9108a5e0f721
Asad Sarfaraz <asadsarfraz474@gmail.com>
2024-09-19 19:38:54 +0530

    Mongoose

--- Day8/server.js
+++ Day8/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
+//Connecting mongodb database with express
+//1) Mongodb driver
+//2) Mongoose
 
-//Connecting mongodb database with express using mongodb driver
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,16 +19,35 @@ app.use(express.urlencoded({ extended: true }));
 
 const port = 8080;
 
-const client = new MongoClient('mongodb://localhost:27017');
-let db;
+// const client = new MongoClient('mongodb://localhost:27017');
+// let db;
 
-client.connect()
-    .then(conn => {
-        console.log(`Connection established to ${conn.s.url}`);
-        db = conn.db('my_db');
-    })
-    .catch(err => console.error(err));
+// client.connect()
+//     .then(conn => {
+//         console.log(`Connection established to ${conn.s.url}`);
+//         db = conn.db('my_db');
+//     })
+//     .catch(err => console.error(err));
     
+let db = mongoose.connect('mongodb://localhost:27017/my_db');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    }, 
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    }
+}, {
+    timestamps: true,
+});
+
+const users = mongoose.model("User", UserSchema);
+
 let contact_obj = { username: "", email: "" };
 
 app.get('/', async(req, res) => {
@@ -48,16 +70,18 @@ app.post('/contact', async(req, res) => {
     // contact_obj = req.body;
     // console.log(contact_obj);
 
-    const collection = await db.collection("users");
-    const results = await collection.insertOne(req.body);
+    // const collection = await db.collection("users");
+    // const results = await collection.insertOne(obj);
+    const results = await users.create(req.body);
     console.log(results);
 
     res.redirect('/user');
 })
 
 app.get('/user', async(req, res) => {
-    const collection = await db.collection("users");
-    const results = await collection.find({}).sort({ $natural: -1 }).limit(1).toArray();
+    // const collection = await db.collection("users");
+    // const results = await collection.find({}).sort({ $natural: -1 }).limit(1).toArray();
+    const results = await users.find({}).sort({ _id: -1 }).limit(1)
     res.render('user', results[0]);
 })
 
